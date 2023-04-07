d2a4c212a11a5da3388052ffbf23908c33754946
BrunoIannone <iannone.1883472@studenti.uniroma1.it>
2023-04-07 12:07:26 +0200

    DataLoader class

---
+++ hw1/stud/utils.py
@@ -0,0 +1,49 @@
+import os
+import json
+EMBEDDING_DIM = 6
+HIDDEN_DIM = 6
+EPOCHS_NUM = 100
+DIRECTORY_NAME = os.path.dirname(__file__)
+
+
+def build_vocabulary(sentences):  # {word:idx}
+    dict = {}
+    idx = 0
+    for sentence in sentences:
+        for token in sentence:
+            if token not in dict:
+                dict[token] = idx
+                idx += 1
+
+    return dict
+
+
+def build_labels(sentences_labels):  # {label:idx}
+    dict = {}
+    idx = 0
+    for labels in sentences_labels:
+        for label in labels:
+            if label not in dict:
+                dict[label] = idx
+                idx += 1
+
+    return dict
+def build_training_data(file_path):
+    f = open(file_path, 'r')
+    line = f.readline()
+    sentences = []
+    labels = []
+    while (line):
+
+        json_line = json.loads(line)
+        sentences.append(json_line['tokens'])
+        labels.append(json_line['labels'])
+        line = f.readline()
+
+    f.close()
+
+    return {
+        'sentences': sentences,
+        'labels': labels
+
+    }
